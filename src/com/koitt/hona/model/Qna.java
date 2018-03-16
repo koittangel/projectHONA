280bba9bb06d8bbb93aa1b50d3cb1d5b6f0d463a
Daniel <Daniel@DESKTOP-MH1PU8R>
2018-03-16 14:53:55 +0900

    projectHONA - Qna 모델 필드명 수정 by krystal

--- src/com/koitt/hona/model/Qna.java
+++ src/com/koitt/hona/model/Qna.java
@@ -12,12 +12,12 @@ public class Qna implements Serializable {
 	private String qnaContent;
 	private Integer userNo;
 	private Date regDate;
-	private String attachment;
+	private String qnaAattachment;
 	
 	public Qna() {}
 
 	public Qna(Integer qnaNo, Integer productNo, String qnaTitle, String qnaContent, Integer userNo, Date regDate,
-			String attachment) {
+			String qnaAattachment) {
 		super();
 		this.qnaNo = qnaNo;
 		this.productNo = productNo;
@@ -25,71 +25,15 @@ public class Qna implements Serializable {
 		this.qnaContent = qnaContent;
 		this.userNo = userNo;
 		this.regDate = regDate;
-		this.attachment = attachment;
-	}
-
-	public Integer getQnaNo() {
-		return qnaNo;
-	}
-
-	public void setQnaNo(Integer qnaNo) {
-		this.qnaNo = qnaNo;
-	}
-
-	public Integer getProductNo() {
-		return productNo;
-	}
-
-	public void setProductNo(Integer productNo) {
-		this.productNo = productNo;
-	}
-
-	public String getQnaTitle() {
-		return qnaTitle;
-	}
-
-	public void setQnaTitle(String qnaTitle) {
-		this.qnaTitle = qnaTitle;
-	}
-
-	public String getQnaContent() {
-		return qnaContent;
-	}
-
-	public void setQnaContent(String qnaContent) {
-		this.qnaContent = qnaContent;
-	}
-
-	public Integer getUserNo() {
-		return userNo;
-	}
-
-	public void setUserNo(Integer userNo) {
-		this.userNo = userNo;
-	}
-
-	public Date getRegDate() {
-		return regDate;
-	}
-
-	public void setRegDate(Date regDate) {
-		this.regDate = regDate;
-	}
-
-	public String getAttachment() {
-		return attachment;
-	}
-
-	public void setAttachment(String attachment) {
-		this.attachment = attachment;
+		this.qnaAattachment = qnaAattachment;
 	}
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((attachment == null) ? 0 : attachment.hashCode());
 		result = prime * result + ((productNo == null) ? 0 : productNo.hashCode());
+		result = prime * result + ((qnaAattachment == null) ? 0 : qnaAattachment.hashCode());
 		result = prime * result + ((qnaContent == null) ? 0 : qnaContent.hashCode());
 		result = prime * result + ((qnaNo == null) ? 0 : qnaNo.hashCode());
 		result = prime * result + ((qnaTitle == null) ? 0 : qnaTitle.hashCode());
@@ -97,6 +41,8 @@ public class Qna implements Serializable {
 		result = prime * result + ((userNo == null) ? 0 : userNo.hashCode());
 		return result;
 	}
+
+	
 
 	@Override
 	public boolean equals(Object obj) {
@@ -131,10 +77,15 @@ public class Qna implements Serializable {
 		builder.append(userNo);
 		builder.append(", regDate=");
 		builder.append(regDate);
-		builder.append(", attachment=");
-		builder.append(attachment);
+		builder.append(", qnaAattachment=");
+		builder.append(qnaAattachment);
 		builder.append("]");
 		return builder.toString();
 	}
+	
+	
+
+	
+
 	
 }
