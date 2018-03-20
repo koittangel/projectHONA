815affb3632dbb7c91e9ef0a20487c6c71c79ad5
KOITT <KOITT@KOITT-PC14>
2018-03-20 09:46:32 +0900

    projectHONA - ddl에 product 테이블에서 EA항목 삭제, INSERT 문에서도 삭제 by brooky

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -38,7 +38,6 @@ CREATE TABLE product(
 	product_type	VARCHAR(30) 	NOT NULL,
 	product_name	VARCHAR(30) 	NOT NULL,
 	price			INT				NOT NULL,
-	EA				INT				NOT NULL,
 	inventory		INT				NOT NULL,
 	explaination	VARCHAR(255)	NOT NULL,
 	attachment		VARCHAR(255)
@@ -94,17 +93,17 @@ INSERT INTO user (id, password, user_name, birth, phone, address)
 	VALUES ('hona3', '$2a$10$DFrlIB4d.kN5cbfIkYDHdO2AXeUV6xtbn1gMT6bTfPDMSJJw6bC.O', '유저2', STR_TO_DATE('1993-01-02', '%Y-%m-%d'), 01011112222, '서울시 금천구');
 
 # 제품 입력
-INSERT INTO product (product_type, product_name, EA, price, inventory, explaination, attachment)
-	VALUES ('TOP', '플란넬 체크 셔츠', 1, 39900, 30, '플란넬 원단으로 제작되어 보온성이 뛰어나며 레이어드용으로, 아우터로, 다양하게 활용하기 좋은 아이템입니다 :)', NULL);
+INSERT INTO product (product_type, product_name, price, inventory, explaination, attachment)
+	VALUES ('TOP', '플란넬 체크 셔츠', 39900, 30, '플란넬 원단으로 제작되어 보온성이 뛰어나며 레이어드용으로, 아우터로, 다양하게 활용하기 좋은 아이템입니다 :)', NULL);
 	
-INSERT INTO product (product_type, product_name, price, EA, inventory, explaination, attachment)
-	VALUES ('상의', '반팔티', 10000, 1, 1, '시원한 반팔티', NULL);
+INSERT INTO product (product_type, product_name, price, inventory, explaination, attachment)
+	VALUES ('상의', '반팔티', 10000, 1, '시원한 반팔티', NULL);
 	
-INSERT INTO product (product_type, product_name, price, EA, inventory, explaination, attachment)
-	VALUES ('하의', '반바지', 20000, 1, 1, '시원한 반바지', NULL);
+INSERT INTO product (product_type, product_name, price, inventory, explaination, attachment)
+	VALUES ('하의', '반바지', 20000, 1, '시원한 반바지', NULL);
 	
-INSERT INTO product (product_type, product_name, price, EA, inventory, explaination, attachment)
-	VALUES ('신발', '슬리퍼', 30000, 1, 1, '시원한 슬리퍼', NULL);
+INSERT INTO product (product_type, product_name, price, inventory, explaination, attachment)
+	VALUES ('신발', '슬리퍼', 30000, 1, '시원한 슬리퍼', NULL);
 
 # 문의글 등록
 INSERT INTO qna (product_no, qna_title, qna_content, user_no, reg_date, qna_attachment)
